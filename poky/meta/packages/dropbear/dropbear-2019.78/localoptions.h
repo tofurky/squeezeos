9f0218a8579c8f2be8401ee0b36be042f0110b32
Ralph Irving <ralph.irving@gmail.com>
2020-12-30 12:57:24 -0500

    Fix dropbear/ssh remote connection disconnects.

--- poky/meta/packages/dropbear/dropbear-2019.78/localoptions.h
+++ poky/meta/packages/dropbear/dropbear-2019.78/localoptions.h
@@ -2,6 +2,6 @@
 #define DROPBEAR_X11FWD 0
 #define DROPBEAR_SFTPSERVER 0
 #define XAUTH_COMMAND "xauth -q"
-#define DEFAULT_KEEPALIVE 1
+#define DEFAULT_KEEPALIVE 0
 #define DO_MOTD 1
 #define DEFAULT_PATH "/sbin:/usr/sbin:/bin:/usr/bin"
