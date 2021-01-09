f235684d7699190a2fe5cdd41e3d0a125c06cc50
Matt Merhar <mattmerhar@protonmail.com>
2021-01-08 21:25:52 -0500

    bump dropbear to 2020.81

---
+++ poky/meta/packages/dropbear/dropbear-2020.81/localoptions.h
@@ -0,0 +1,7 @@
+#define NON_INETD_MODE 0
+#define DROPBEAR_X11FWD 0
+#define DROPBEAR_SFTPSERVER 0
+#define XAUTH_COMMAND "xauth -q"
+#define DEFAULT_KEEPALIVE 0
+#define DO_MOTD 1
+#define DEFAULT_PATH "/sbin:/usr/sbin:/bin:/usr/bin"
