8773d7fe900e6c0b0f7b2417b113f21b7445020d
Ralph Irving <ralph.irving@gmail.com>
2021-03-07 15:01:54 -0500

    Reduce the risk of denial of service by closing an unauthenticated dropbear/ssh session immediately.

--- poky/meta/packages/dropbear/dropbear-2019.78/localoptions.h
+++ poky/meta/packages/dropbear/dropbear-2019.78/localoptions.h
@@ -3,5 +3,6 @@
 #define DROPBEAR_SFTPSERVER 0
 #define XAUTH_COMMAND "xauth -q"
 #define DEFAULT_KEEPALIVE 0
+#define UNAUTH_CLOSE_DELAY 0
 #define DO_MOTD 1
 #define DEFAULT_PATH "/sbin:/usr/sbin:/bin:/usr/bin"
